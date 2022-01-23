abdee0620734f41f070a8b820b777e196c5d85db
matten-rd <strandfilip21@gmail.com>
2022-01-23 17:10:00 +0100

    draw circle on click event and clear circles feature

--- angleFinder.py
+++ angleFinder.py
@@ -3,12 +3,17 @@ import math
 
 path = "test.jpg"
 img = cv2.imread(path)
+points = [] # Store coordinates of points in 2D-list
 
 def mousePoints(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, y)
+        cv2.circle(img, (x, y), 5, (0,0,255), cv2.FILLED)
+        points.append([x, y])
+        print(points)
 
-
-cv2.imshow("Image", img)
-cv2.setMouseCallback("Image", mousePoints)
-cv2.waitKey(0)
+while True:
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mousePoints)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        points = [] # Clear points if user types q
+        img = cv2.imread(path)
